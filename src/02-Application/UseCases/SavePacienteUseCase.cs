985edfd1eea55c2406c55f1cbc2f4dae0aaa6a0c
Ricardo Fulgencio Alves <ricardo.fulgencio@hotmail.com>
2025-02-07 20:42:22 -0300

    feat: Configuração de hash para CPF e TODO para consumo de serviço para inclusão de autenticação

--- src/02-Application/UseCases/SavePacienteUseCase.cs
+++ src/02-Application/UseCases/SavePacienteUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using PosTech.GrupoOito.Hackathon.PacienteManagement.Events;
+using PosTech.Hackathon.Pacientes.Application.Helpers;
 using PosTech.Hackathon.Pacientes.Domain.Entities;
 using PosTech.Hackathon.Pacientes.Domain.Interfaces;
 using PosTech.Hackathon.Pacientes.Domain.Responses;
@@ -15,19 +16,21 @@ public class SavePacienteUseCase : ISavePacienteUseCase
         _pacienteRepository = pacienteRepository;
     }
 
-    public async Task Execute(Paciente paciente)
-    {
-        await _pacienteRepository.AddPacienteAsync(paciente);
-    }
-
     public async Task<DefaultOutput<PacienteResponse>> SaveNewPacienteAsync(CreatePacienteEvent request)
     {
+        var createUserAutentication = Guid.NewGuid(); //consumir serviço Authentication e retornar Id para persistir em Collection de Paciente;
+
+        if (createUserAutentication == Guid.Empty) 
+        { 
+            return new DefaultOutput<PacienteResponse>(System.Net.HttpStatusCode.UnprocessableEntity, false, $"Erro ao cadastrar novo paciente."); 
+        }
+
         var paciente = new Paciente
         {
+            Id = createUserAutentication,
             Nome = request.Nome,
-            Cpf = NormalizeCpf(request.Cpf),
+            Cpf = HashHelper.ComputeSha256Hash(NormalizeCpf(request.Cpf)),
             Email = request.Email,
-            Senha = request.Senha
         };
 
         await _pacienteRepository.AddPacienteAsync(paciente);
--- src/03-Domain/Entities/Paciente.cs
+++ src/03-Domain/Entities/Paciente.cs
@@ -4,9 +4,8 @@ namespace PosTech.Hackathon.Pacientes.Domain.Entities;
 
 public class Paciente
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public Guid Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public string Cpf { get; set; }
     public string Email { get; set; } = string.Empty;
-    public string Senha { get; set; }
 }
